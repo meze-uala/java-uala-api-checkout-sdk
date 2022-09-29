3fb534cb1c4ddb99e3b3604354df14efb4dd1f4a
Meze <martin.abogado@uala.com.ar>
2022-09-29 16:17:12 -0300

    Add failed notifications sample and rename variables.

--- src/main/java/samples/Main.java
+++ src/main/java/samples/Main.java
@@ -19,23 +19,23 @@ public class Main {
         //You can also create an instance of client with others constructors.
         //The other one is passing the environment as String parameter
 
-        System.out.println("Environment of client2: "+ client.getConfig().getEnvironment());
+        System.out.println("Environment of client: "+ client.getConfig().getEnvironment());
 
         //And the last one is creating a config and pass it as parameter
         Config sampleConfig = new Config("stage");
         Client client2 = new Client(sampleConfig);
-        System.out.println("Environment of client3: "+client2.getConfig().getEnvironment());
+        System.out.println("Environment of client2: "+client2.getConfig().getEnvironment());
 
         //Prepare the request
         AuthRequest req = new AuthRequest("javauser", "qSBRbtObFn5GJJnYvm2M3pSn13jgHPMN",
                 "vVvbYMTmNKggv11naxMfbZ7qbdo6SKS985SwZYE0FSsfewNMKXwpzxemr6DKoQ-8");
 
         //Get the authToken (necessary for create and retrieve orders)
-        String respuesta = client.getAuthToken(req);
+        String token = client.getAuthToken(req);
         System.out.println(">>>>>>>>>>>>>>>>>");
         System.out.println("Getting authToken -->");
         System.out.println("AuthToken response -->");
-        System.out.println(respuesta);
+        System.out.println(token);
 
         //Prepare CreateOrder (also know as 'checkout') request
         OrderRequest or = new OrderRequest("1986.10", "Copa Mundial Original", "javauser",
@@ -48,18 +48,26 @@ public class Main {
 
         System.out.println(">>>>>>>>>>>>>>>>>");
         System.out.println("Creating checkout order -->");
-        String respuesta2 = client.createOrder(or);
+        String createdOrder = client.createOrder(or);
         System.out.println("Order response -->");
-        System.out.println(respuesta2);
+        System.out.println(createdOrder);
         System.out.println("");
 
 
         System.out.println(">>>>>>>>>>>>>>>>>");
         System.out.println("Getting an order -->");
         String orderID = "82b41bb1-ae75-40a0-a3c7-ffb35903a484";
-        String respuesta3 = client.getOrder(orderID);
+        String order = client.getOrder(orderID);
         System.out.println("Order retrieved -->");
-        System.out.println(respuesta3);
+        System.out.println(order);
+        System.out.println("");
+
+
+        System.out.println(">>>>>>>>>>>>>>>>>");
+        System.out.println("Getting failed notifications -->");
+        String failedNotifications = client.getFailedNotifications();
+        System.out.println("Failed notifications retrieved -->");
+        System.out.println(failedNotifications);
         System.out.println("");
 
     }
